69ec10021ca25b8fd954dcce9e9b21c34fec9daf
Suhayl <895761900@qq.com>
2025-03-14 14:12:38 +0800

    udpate

--- docs/source/_static/js/footer_custom.js
+++ docs/source/_static/js/footer_custom.js
@@ -1,14 +1,4 @@
-﻿let footer_html = `
-<div style="border-top: 1px solid #ccc; ">
-    <br/>
-    <div id="copy_right" style="float: left;">
-        © CopyRight 2016 - 2025
-    </div>
-    <div style="float: right;">
-        Need help? Contact <a href="mailto:support@freenove.com">support@freenove.com</a> 
-    </div>
-    <br/>
-</div>`;
+﻿
 
 
 window.onload = function () {
